60b1b76ac31b554bc3fde1ec6bdd863cea8e24ff
timavik <103438439+timavik@users.noreply.github.com>
2024-12-23 16:15:48 +0300

    Add files via upload

---
+++ Lesson5Dz/Lesson5Dz/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson5DzOne
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Book book1 = new Book("Разгавор с мумией", "Эдгард Алон По", 16);
+      Console.WriteLine(book1.GetDescription());
+
+      Book book2 = new Book("Алхимик", "Пауло Коэльо", 231);
+      Console.WriteLine(book2.GetDescription());
+    }
+  }
+}
